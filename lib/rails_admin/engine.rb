593bb22ac660bb918678b5d6c93f8d0daff30bec
Marcin Seroczynski <marcin@seroczynski.net>
2012-04-24 20:58:21 +0200

    Removed asset pipeline configuration from engine.

--- lib/rails_admin/engine.rb
+++ lib/rails_admin/engine.rb
@@ -11,9 +11,6 @@ require 'rails_admin'
 module RailsAdmin
   class Engine < Rails::Engine
     isolate_namespace RailsAdmin
-    initializer "RailsAdmin precompile hook" do |app|
-      app.config.assets.precompile += ['rails_admin/rails_admin.js', 'rails_admin/rails_admin.css', 'rails_admin/jquery.colorpicker.js', 'rails_admin/jquery.colorpicker.css']
-    end
 
     initializer "RailsAdmin pjax hook" do |app|
       app.config.middleware.use Rack::Pjax
